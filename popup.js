8b17afac49e0c4d81f3fb5604b1c12d9c70bb989
RiskyFritz <67400243+RiskyFritz@users.noreply.github.com>
2021-03-24 01:46:26 -0700

    added symbols

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 let password = '';
 const letters = ['A','B','C', 'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y','z']
 const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-const symbols = ['!', '#', '$', '%', '^', '&', '?'];
+const symbols = ['!', '#', '$', '%', '^', '&', '?', '@', '(', ')', '-', '_', '=', '+', ',',  '<', '>'];
 const letNum = [...new Set([...letters, ...numbers])];
 const letSym = [...new Set([...letters, ...symbols])];
 const numSym = [...new Set([...numbers, ...symbols])];
