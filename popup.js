739290d4dc993603de8062245204745aed22f556
RiskyFritz <67400243+RiskyFritz@users.noreply.github.com>
2021-03-24 01:40:46 -0700

    Added functionality for check boxes

--- popup.js
+++ popup.js
@@ -1,68 +1,12 @@
-//popup.js
-
 let password = '';
-const letters = [
-	'A',
-	'B',
-	'C',
-	'D',
-	'E',
-	'F',
-	'G',
-	'H',
-	'I',
-	'J',
-	'K',
-	'L',
-	'M',
-	'N',
-	'O',
-	'P',
-	'Q',
-	'R',
-	'S',
-	'T',
-	'U',
-	'V',
-	'W',
-	'X',
-	'Y',
-	'Z',
-	'a',
-	'b',
-	'c',
-	'd',
-	'e',
-	'f',
-	'g',
-	'h',
-	'i',
-	'j',
-	'k',
-	'l',
-	'm',
-	'o',
-	'p',
-	'q',
-	'r',
-	's',
-	't',
-	'u',
-	'v',
-	'w',
-	'x',
-	'y',
-	'z',
-];
+const letters = ['A','B','C', 'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y','z']
 const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const symbols = ['!', '#', '$', '%', '^', '&', '?'];
-let charSet = [];
+const letNum = [...new Set([...letters, ...numbers])];
+const letSym = [...new Set([...letters, ...symbols])];
+const numSym = [...new Set([...numbers, ...symbols])];
+const numSymLet = [...new Set([...letters, ...numbers, ...symbols])];
 
-function createCharSet() {
-	charSet = [...new Set([...letters, ...numbers, ...symbols])];
-
-	return charSet;
-}
 
 function generatePassword(length, characterSet) {
 	password = '';
@@ -84,12 +28,14 @@ document.addEventListener(
 		const copyPassword = document.getElementById('copyPassword');
 		const faCopy = document.querySelector('.fa.fa-clone');
 		const darkModeToggle = document.getElementById('darkModeToggle');
+		let charSet = [];
 
 		sliderValue.innerHTML = slider.value;
 
 		slider.oninput = function () {
 			sliderValue.innerHTML = this.value;
 		};
+
 		// --- add password to user's clipboard ---
 		function updateClipboard(clipboardText) {
 			navigator.clipboard.writeText(clipboardText).then(
@@ -110,6 +56,33 @@ document.addEventListener(
 		copyPassword.addEventListener('click', function () {
 			updateClipboard(password);
 		});
+
+		// --- function to check password options and create character Set ---
+		function createCharSet() {
+			let lettersCheck = document.getElementById('checkLetters').checked;
+			let numbersCheck = document.getElementById('checkNumbers').checked;
+			let symbolsCheck = document.getElementById('checkSymbols').checked;
+			charSet = [];
+
+			if (lettersCheck === true && numbersCheck === true && symbolsCheck === true) {
+				charSet = numSymLet;
+			} else if (lettersCheck === true && numbersCheck === true && symbolsCheck === false) {
+				charSet = letNum;
+			} else if (lettersCheck === true && numbersCheck === false && symbolsCheck == true) {
+				charSet = letSym;
+			} else if (lettersCheck === false && numbersCheck === true && symbolsCheck === true) {
+				charSet = numSym;
+			} else if (lettersCheck === true && numbersCheck === false && symbolsCheck === false) {
+				charSet = letters;
+			} else if (lettersCheck === false && numbersCheck === true && symbolsCheck === false) {
+				charSet = numbers;
+			} else if (lettersCheck === false && numbersCheck === false && symbolsCheck === true) {
+				charSet = symbols;
+			} 
+			
+			return charSet;
+		}
+		
 		// --- listen for generate password click ---
 		generatePasswordButton.addEventListener(
 			'click',
