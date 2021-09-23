d465f60fcd3740282f78ff2b49d08dd017070c11
Zach <zdennis27@gmail.com>
2021-09-22 18:30:14 -0700

    Added content script functionality to add images

--- contentScript.js
+++ contentScript.js
@@ -1,14 +1,22 @@
 // Checking page title
 
 let email = document.getElementById('email');
-console.log(email)
+let password = document.getElementById('pass');
+let imgurl = chrome.extension.getURL("images/icon.png");
 
+email.src = imgurl;
+email.style.backgroundImage.src = imgurl; 
+email.style.backgroundImage = 'url("chrome-extension://icmplkgcfcfmhiebkaeljdogfnnipcno/images/icon.png")';
 email.style.backgroundRepeat = 'no-repeat';
-email.style.backgroundImage = "url('https://sebhastian.com/img/default.png')"; 
-email.style.background = 'red';
 email.style.backgroundAttachment = "scroll"; 
 email.style.backgroundSize = "16px 18px"; 
 email.style.backgroundPosition = "98% 50%"; 
 email.style.cursor = "pointer";
-console.log(email.style);
-console.log(email.style.backgroundImage);
+
+password.style.backgroundImage.src = imgurl; 
+password.style.backgroundImage = 'url("chrome-extension://icmplkgcfcfmhiebkaeljdogfnnipcno/images/icon.png")';
+password.style.backgroundRepeat = 'no-repeat';
+password.style.backgroundAttachment = "scroll"; 
+password.style.backgroundSize = "16px 18px"; 
+password.style.backgroundPosition = "98% 50%"; 
+password.style.cursor = "pointer";
