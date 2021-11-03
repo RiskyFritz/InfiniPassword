159396b34e0699ca5d2a70c857acf080dbcf5d72
Ethan Olsen <ethan@crowdhubapps.com>
2021-11-02 18:13:10 -0700

    fix: route to / on mount if pathname == index.html

--- src/pages/__layout.tsx
+++ src/pages/__layout.tsx
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router'
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const __layout: React.FC = ({ children }) => {
 	// ---- hooks ----
 	// > state
 	const [count, setCount] = useState(0);
+	// > router
+	const history = useHistory();
+
+	// ---- lifecycle ----
+	// onMount
+	useEffect(() => {
+		if (location.pathname === '/index.html') {
+			history.push('/');
+		}
+	},[])
 
 	return (
 		<div>
