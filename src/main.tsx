2e2cc5cf9f84a56c361da8f7dcbb99c607294439
Ethan Olsen <ethan@crowdhubapps.com>
2021-12-08 18:00:36 -0800

    feat: add tailwindcss and postcss

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Routes } from './routes';
 import DarkModeContextProvider from './contexts/DarkModeContext';
+import 'tailwindcss/tailwind.css';
 
 ReactDOM.render(
 	<React.StrictMode>
